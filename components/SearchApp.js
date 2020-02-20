37c9c675e7d4e7cf4bfb3e5778f815697e1bb0f7
hentschellj <hentschellj@gmail.com>
2020-02-19 20:14:54 -0600

    Created primary component

---
+++ components/SearchApp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SearchApp extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        Search App
+      </div>
+    )
+  }
+}
+
+export default SearchApp;
--- main/index.js
+++ main/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchApp from '../components/SearchApp';
 
 function App() {
   return (
     <div>
-      App
+      <SearchApp />
     </div>
   )
 }
