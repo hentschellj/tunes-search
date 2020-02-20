b7a5280bdac5d931d07a3a06ebd5b4efbfa4aaa6
hentschellj <hentschellj@gmail.com>
2020-02-19 20:27:18 -0600

    Form capturing text

--- components/SearchForm.js
+++ components/SearchForm.js
@@ -3,13 +3,26 @@ import React from 'react';
 class SearchForm extends React.Component {
   constructor() {
     super();
+
+    this.state = {
+      text: ''
+    }
+
+    this.updateText = this.updateText.bind(this)
+  }
+
+  updateText(e) {
+    this.setState({
+      text: e.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <form className="input-group input-group-lg">
-          <input type="text" className="form-control" />
+          {this.state.text}
+          <input className="form-control" type="text" onChange={this.updateText} value={this.state.text} />
           <span className="input-group-btn">
             <input className="btn btn-default" type="button" value="Search" />
           </span>
