c47dc59d3943e5e12400de16be4e14b165e39fca
hentschellj <hentschellj@gmail.com>
2020-02-19 21:06:31 -0600

    Created layout for displaying results

--- components/SearchResult.js
+++ components/SearchResult.js
@@ -4,7 +4,19 @@ class SearchResult extends React.Component {
   render() {
     return (
       <div>
-        Search Results
+        <li className="media">
+          <a href={props.item.trackViewUrl} target="_blank">
+            <div className="media-left">
+              <img src={props.item.artworkUrl60} />
+            </div>
+
+            <div className="media-body">
+              <h4 className="media-heading">{props.item.trackName}</h4>
+              <em>{props.item.artistName}</em>
+              <span className="badge pullRight">{props.item.currency} {props.item.trackPrice}</span>
+            </div>
+          </a>
+        </li>
       </div>
     )
   }
