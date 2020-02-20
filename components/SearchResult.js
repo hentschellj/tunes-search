f7e06346ed5cdc449eb35c54d666813c3b6e4850
hentschellj <hentschellj@gmail.com>
2020-02-19 21:07:07 -0600

    Changed SearchResult into function, fixing error

--- components/SearchResult.js
+++ components/SearchResult.js
@@ -1,25 +1,23 @@
 import React from 'react';
 
-class SearchResult extends React.Component {
-  render() {
-    return (
-      <div>
-        <li className="media">
-          <a href={props.item.trackViewUrl} target="_blank">
-            <div className="media-left">
-              <img src={props.item.artworkUrl60} />
-            </div>
+function SearchResult(props) {
+  return (
+    <div>
+      <li className="media">
+        <a href={props.item.trackViewUrl} target="_blank">
+          <div className="media-left">
+            <img src={props.item.artworkUrl60} />
+          </div>
 
-            <div className="media-body">
-              <h4 className="media-heading">{props.item.trackName}</h4>
-              <em>{props.item.artistName}</em>
-              <span className="badge pullRight">{props.item.currency} {props.item.trackPrice}</span>
-            </div>
-          </a>
-        </li>
-      </div>
-    )
-  }
+          <div className="media-body">
+            <h4 className="media-heading">{props.item.trackName}</h4>
+            <em>{props.item.artistName}</em>
+            <span className="badge pullRight">{props.item.currency} {props.item.trackPrice}</span>
+          </div>
+        </a>
+      </li>
+    </div>
+  )
 }
 
 export default SearchResult;
